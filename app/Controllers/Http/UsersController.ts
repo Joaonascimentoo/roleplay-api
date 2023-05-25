01041aa3874d32541645235b476daae2f58fc36f
Joao Muniz <jpedro747.jp@mail.com>
2023-05-25 08:56:46 -0300

    feat: add uptdate method

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,15 +1,33 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import BadRequest from 'App/Exceptions/BadRequestException'
 import User from 'App/Models/User'
+import CreateUser from 'App/Validators/CreateUserValidator'
 
 export default class UsersController {
   public async store({ request, response }: HttpContextContract) {
-    const userPayload = request.only(['email', 'username', 'password', 'avatar'])
+    const userPayload = await request.validate(CreateUser)
+
     const userByEmail = await User.findBy('email', userPayload.email)
+    const userByName = await User.findBy('username', userPayload.username)
 
     if (userByEmail) throw new BadRequest('email already in use', 409)
+    if (userByName) throw new BadRequest('username already in use', 409)
 
     const user = await User.create(userPayload)
     return response.created({ user })
+  }
+
+  public async update({ request, response }: HttpContextContract) {
+    const { email, password, avatar } = request.only(['email', 'avatar', 'password'])
+    const id = request.param('id')
+
+    const user = await User.findOrFail(id)
+
+    user.email = email
+    user.password = password
+    if (avatar) user.avatar = avatar
+    await user.save()
+
+    return response.ok({ user })
   }
 }
